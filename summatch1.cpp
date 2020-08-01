e5994f218034a2fbbf7d57d8000d1616308cc85c
Sharanya S Bhat <59570348+Sharanya-create@users.noreply.github.com>
2020-08-01 23:27:06 +0530

    Create summatch1.cpp

---
+++ summatch1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+typedef long long int l;
+int main(){
+	l n;
+	int found=0;
+	cin>>n;
+	if(n==1){
+		cout<<"["<<2<<","<<-1<<"]"<<endl;
+	}
+	else{
+	
+    cout<<"["<<1<<","<<n-1<<"]"<<endl;
+}
+		
+	
+}
